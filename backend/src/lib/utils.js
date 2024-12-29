f5391310f9396b15e27dbb97f8f5ebe506c3e725
pg147 <xyzprathamesh147@gmail.com>
2024-12-29 22:23:09 +0530

    JWT Token generation logic added

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+    if (token) {
+        res.cookie("us_er_tok_en", token, {
+            maxAge: 24 * 7 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV !== "development"
+        })
+    } else {
+        res.status(400).json({ message : "Error generating token!" });
+        return;
+    }
+
+    return token;
+}
