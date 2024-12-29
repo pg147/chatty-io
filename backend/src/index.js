0d5ff4894fb8b501dc86fd0e5df8e4394ae21c0e
pg147 <xyzprathamesh147@gmail.com>
2024-12-29 19:55:16 +0530

    Database connection logic added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from 'express'; // express module
+import dotenv from 'dotenv';
+
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
 
 const app = express(); // initiating express app
-const PORT = 5001; // defined PORT 
+const PORT = process.env.PORT; // defined PORT 
 
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running at : ${PORT}`);
+    connectDB();
 })
