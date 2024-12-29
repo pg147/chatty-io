8c1d75e5acd3c95b67bbfd3621240628b3c4cb90
pg147 <xyzprathamesh147@gmail.com>
2024-12-29 21:57:24 +0530

    Salt and Hash logic added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,28 @@
+import User from "../models/users.model.js";
+import bcrypt from "bcryptjs";
+
 // Signup 
-export const signup = (req, res) => {
-    res.send("Signup route");
+export const signup = async (req, res) => {
+    const { name, email, password, profilePic } = req.body;
+
+    try {
+        // Validating password length
+        if (password.length < 6) {
+            res.status(400).json({ message : "Password length should be greater than 6" });
+        }
+
+        // Checking if user already exists
+        const user = await User.findOne({ email });
+
+        // If exists then throw alert
+        if(user) res.status(400).json({ message : `User with email ${email} already exists` });
+
+        const salt = await bcrypt.genSalt(10); // Creating a salt for password
+        const hashedPassword = await bcrypt.hash(password, salt); // Hashing the password with generated salt
+
+    } catch (error) {
+        
+    }
 }
 
 // Login
