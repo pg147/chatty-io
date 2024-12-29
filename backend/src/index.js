9920c06b11e8081d6204a701e278b4d0ad97eb57
pg147 <xyzprathamesh147@gmail.com>
2024-12-29 21:57:06 +0530

    Json middleware applied

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,18 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 
-dotenv.config();
+dotenv.config(); // For using env variables
 
-const app = express(); // initiating express app
+const app = express(); // initialized express app
 const PORT = process.env.PORT; // defined PORT 
 
+// JSON Middleware
+app.use(express.json());
+
+// Route
 app.use('/api/auth', authRoutes);
 
+// Server
 app.listen(PORT, () => {
     console.log(`Server running at : ${PORT}`);
     connectDB();
