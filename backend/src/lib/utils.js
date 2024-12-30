52c377265c9327a92c4f54a6f19f9619d0faaa7f
pg147 <xyzprathamesh147@gmail.com>
2024-12-31 00:51:13 +0530

    Cookie variable name changed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,7 +4,7 @@ export const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
 
     if (token) {
-        res.cookie("us_er_tok_en", token, {
+        res.cookie("user", token, {
             maxAge: 24 * 7 * 60 * 60 * 1000,
             httpOnly: true,
             sameSite: "strict",
