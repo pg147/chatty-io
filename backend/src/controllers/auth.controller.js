a349732defdaca97d436e93e2bc86dbc8d41c386
pg147 <xyzprathamesh147@gmail.com>
2024-12-31 00:51:32 +0530

    CheckAuth function added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -115,3 +115,12 @@ export const update = async (req, res) => {
         res.status(500).json({ message: "Internal server error."});
     }   
 }
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error checking user : ", error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 // Controller Logics
-import { signup, login, logout, update } from '../controllers/auth.controller.js';
+import { signup, login, logout, update, checkAuth } from '../controllers/auth.controller.js';
 
 const router = express.Router(); // Initialized Router
 
@@ -11,6 +11,9 @@ const router = express.Router(); // Initialized Router
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
+
 router.put('/update-profile', protectRoute, update);
+
+router.get('/check', protectRoute, checkAuth);
 
 export default router;
