d4a0137f660393306880c20e5e4f607d30639dd3
pg147 <xyzprathamesh147@gmail.com>
2024-12-31 00:07:12 +0530

    Auth route updated for UpdateProfile

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 // Express module
 import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 // Controller Logics
-import { signup, login, logout } from '../controllers/auth.controller.js';
+import { signup, login, logout, update } from '../controllers/auth.controller.js';
 
 const router = express.Router(); // Initialized Router
 
@@ -10,5 +11,6 @@ const router = express.Router(); // Initialized Router
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
+router.put('/update-profile', protectRoute, update);
 
 export default router;
