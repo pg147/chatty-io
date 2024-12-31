fa03d8c02785e116d21024938da8e3a7dcf81a50
pg147 <xyzprathamesh147@gmail.com>
2024-12-31 20:34:48 +0530

    GetMessages function logic added

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/users.model.js";
+import Message from "../models/messages.model.js";
 
 export const getUsersForSideBar = async (req, res) => {
     try {
@@ -17,5 +18,29 @@ export const getUsersForSideBar = async (req, res) => {
     } catch (error) {
         console.log("Error finding users ", error);
         res.status(500).json({message: "Internal server error."});
+    }
+}
+
+export const getMessages = async (req, res) => {
+    const myId = req.user._id;
+    const { id: userToChat} = req.params;
+    
+    try {
+        const messages = await Message.find({
+            // Or condition -> For fetching msgs where either am the sender or receiver and vice-versa
+            $or: [
+                { senderId: myId, receiverId: userToChat }, 
+                { senderId: userToChat, receiverId: myId }
+            ]
+        });
+
+        if (!messages) {
+            return res.status(400).json({ message: "No messages were found !"});
+        }
+
+        res.status(201).json(messages);
+    } catch (error) {
+        console.log('Error fetching messages : ', error);
+        res.status(500).json({ message: "Internal server error!" });
     }
 }
