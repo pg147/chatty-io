f693ce820fa59afb119dd02f599d50f801446b28
pg147 <xyzprathamesh147@gmail.com>
2025-01-01 22:58:01 +0530

    Daisy UI installed and configured

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,7 @@
+import daisyui from "daisyui"
+
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +10,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 }
 
