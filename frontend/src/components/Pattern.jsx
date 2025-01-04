413aea785779a8dd08c4ee994ee1b9877d4d9abc
pg147 <xyzprathamesh147@gmail.com>
2025-01-04 23:55:06 +0530

    Pulse animation added for Pattern

--- frontend/src/components/Pattern.jsx
+++ frontend/src/components/Pattern.jsx
@@ -3,7 +3,7 @@ export default function Pattern() {
   return (
     <div className="grid grid-cols-3 gap-2">
       {Array.from({ length: 9 }).map((boxes, index) => (
-        <div key={index} className={`size-28 rounded-2xl ${(index + 1) % 2 === 0 ? 'bg-primary/10' : 'bg-white border-2 border-primary'}`}>
+        <div key={index} className={`size-28 rounded-2xl ${(index + 1) % 2 === 0 ? 'bg-primary/10 animate-pulse' : 'bg-white border-2 border-primary'}`}>
 
         </div>
       ))}
