944a01cf1ab6a56148933a10583f9d58b1afeb02
pg147 <xyzprathamesh147@gmail.com>
2025-01-05 01:05:22 +0530

    Conditional rendering for Profile and Settings at Navbar and comments for better understanding

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+// Global States
+import { useAuthStore } from "../store/useAuthStore";
+
+// Icon Library
 import { LucideLogOut, LucideMessageSquare, LucideSettings, LucideUser2 } from "lucide-react";
 
 export default function Navbar() {
+  const { logout, authUser } = useAuthStore();
+
   return (
     <div className="h-fit w-full z-[10] flex items-center justify-between px-4 py-3 lg:px-8 lg:py-6 shadow-intense ">
       {/* Brand */}
@@ -17,28 +23,30 @@ export default function Navbar() {
       {/* User Controls */}
       <div className="flex w-fit gap-x-3">
         {/* Settings */}
-        <div className="lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
+        <div className="cursor-pointer lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
           <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
             <LucideSettings className="size-6 lg:size-5 text-primary" />
           </div>
           <h1 className="font-medium hidden lg:block">Settings</h1>
         </div>
 
-        {/* Profile */}
-        <div className="lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
-          <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
-            <LucideUser2 className="size-6 lg:size-5 text-primary" />
+        {authUser && <>
+          {/* Profile */}
+          <div className="cursor-pointer lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
+            <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
+              <LucideUser2 className="size-6 lg:size-5 text-primary" />
+            </div>
+            <h1 className="font-medium hidden lg:block">Profile</h1>
           </div>
-          <h1 className="font-medium hidden lg:block">Profile</h1>
-        </div>
 
-        {/* Logout */}
-        <div className="lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
-          <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
-            <LucideLogOut className="size-6 lg:size-5 text-primary" />
+          {/* Logout */}
+          <div onClick={logout} className="cursor-pointer lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
+            <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
+              <LucideLogOut className="size-6 lg:size-5 text-primary" />
+            </div>
+            <h1 className="font-medium hidden lg:block">Logout</h1>
           </div>
-          <h1 className="font-medium hidden lg:block">Logout</h1>
-        </div>
+        </>}
       </div>
     </div>
   )
