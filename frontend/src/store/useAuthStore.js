b763de0a54c1bcc9539a958ab27caea497a66aab
pg147 <xyzprathamesh147@gmail.com>
2025-01-05 00:30:51 +0530

    Signup function updated and Logout function added

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios';
+import toast from 'react-hot-toast';
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -24,11 +25,24 @@ export const useAuthStore = create((set) => ({
     },
 
     signUp: async (data) => {
+        set({ isSigningUp: true });
         try {
             const res = await axiosInstance.post('/auth/signup', data);
-            
+            set({ authUser : res.data });
+            toast.success("Account created successfully!");
         } catch (error) {
-            console.log('Error signin up : ', error.message);
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post('/auth/logout');
+            set({ authUser: null });
+        } catch (error) {
+            toast.error(error.response.data.message);
         }
     }
 }));
