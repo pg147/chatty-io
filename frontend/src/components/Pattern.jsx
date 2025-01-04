02bad5c9c126a5568bca519f4411b0a90de6cc54
pg147 <xyzprathamesh147@gmail.com>
2025-01-04 21:55:40 +0530

    Pattern Design created for Auth pages

---
+++ frontend/src/components/Pattern.jsx
@@ -0,0 +1,12 @@
+
+export default function Pattern() {
+  return (
+    <div className="grid grid-cols-3 gap-2">
+      {Array.from({ length: 9 }).map((boxes, index) => (
+        <div key={index} className={`size-28 rounded-2xl ${(index + 1) % 2 === 0 ? 'bg-primary/10' : 'bg-white border-2 border-primary'}`}>
+
+        </div>
+      ))}
+    </div>
+  )
+}
