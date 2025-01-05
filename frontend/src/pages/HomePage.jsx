bbc1af138d2316da117a65f89c3d6d276f2330d7
pg147 <xyzprathamesh147@gmail.com>
2025-01-05 23:17:34 +0530

    Updated - Homepage : Sidebar & ChatInterface

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,25 @@
+// Global States
+import { useChatStore } from "../store/useChatStore"
+
+// Components
+import Sidebar from "../components/Sidebar";
+import ChatInterface from "../components/ChatInterface";
+import NoSelectedUser from "../components/NoSelectedUser";
 
 export default function HomePage() {
+  const { selectedUser } = useChatStore();
+
   return (
-    <div>Homepage</div>
+    <div className="w-full h-screen md:flex">
+      {/* Sidemenu - Users */}
+      <div className="h-screen w-full md:w-[30%] lg:w-[20%]">
+        <Sidebar />
+      </div>
+
+      {/* Chat Interface */}
+      <div className="md:w-[70%] lg:w-[80%] bg-light hidden md:flex flex-col items-center justify-center">
+        {selectedUser ? <ChatInterface /> : <NoSelectedUser /> }
+      </div>
+    </div>
   )
 }
