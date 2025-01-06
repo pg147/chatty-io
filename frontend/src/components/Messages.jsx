c6b88490404b48dc72ec26a487b015a98cecbe88
pg147 <xyzprathamesh147@gmail.com>
2025-01-06 21:42:09 +0530

    Updated - Messages: 0 Messages Screen

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,11 +1,12 @@
 // Global States
+import { LucideMessageCircle } from "lucide-react";
 import { useChatStore } from "../store/useChatStore";
 
 // Skeleton Components
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 export default function Messages() {
-    const { isMessagesLoading, messages } = useChatStore();
+    const { isMessagesLoading, messages, selectedUser } = useChatStore();
 
     return (
         <>
@@ -15,8 +16,11 @@ export default function Messages() {
                 </div>
             ) : (
                 messages.length === 0 ? (
-                    <div className="flex flex-col h-screen items-center justify-center">
-
+                    <div className="flex flex-col space-y-4 h-screen items-center justify-center">
+                        <div className="size-fit bg-primary/10 rounded-full p-5 animate-bounce">
+                            <LucideMessageCircle className="size-9 text-primary" />
+                        </div>
+                        <h1 className="text-center text-lg">Type &quot;Hi&quot; to start a conversation with <span className="font-semibold">{`${selectedUser.name.slice(0,1).toUpperCase()}${selectedUser.name.slice(1)}`}</span></h1>
                     </div>
                 ) : (
                     <div>
