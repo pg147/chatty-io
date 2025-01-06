886808456359ce375db0b6b0ab897edd8a8c584c
pg147 <xyzprathamesh147@gmail.com>
2025-01-07 01:31:05 +0530

    Updated : Message Interface

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,15 +1,19 @@
 // Global States
-import { LucideMessageCircle } from "lucide-react";
 import { useChatStore } from "../store/useChatStore";
+import { useAuthStore } from "../store/useAuthStore";
+
+// Icon Library
+import { LucideMessageCircle } from "lucide-react";
 
 // Skeleton Components
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 export default function Messages() {
     const { isMessagesLoading, messages, selectedUser } = useChatStore();
+    const { authUser } = useAuthStore();
 
     return (
-        <>
+        <div className="h-screen bg-light">
             {isMessagesLoading ? (
                 <div className="flex flex-col h-screen">
                     <MessageSkeleton />
@@ -20,14 +24,59 @@ export default function Messages() {
                         <div className="size-fit bg-primary/10 rounded-full p-5 animate-bounce">
                             <LucideMessageCircle className="size-9 text-primary" />
                         </div>
-                        <h1 className="text-center text-lg">Type &quot;Hi&quot; to start a conversation with <span className="font-semibold">{`${selectedUser.name.slice(0,1).toUpperCase()}${selectedUser.name.slice(1)}`}</span></h1>
+                        <h1 className="text-center text-lg">Type &quot;Hi&quot; to start a conversation with <span className="font-semibold">{`${selectedUser.name.slice(0, 1).toUpperCase()}${selectedUser.name.slice(1)}`}</span></h1>
                     </div>
                 ) : (
-                    <div>
-
+                    <div className="flex-1 flex flex-col p-9">
+                        {messages.map((message, index) => (
+                            authUser._id === message.senderId ? (
+                                <div
+                                    key={index}
+                                    className={`w-full flex justify-end`}
+                                >
+                                    <div className="w-fit flex gap-x-4 flex-row-reverse mb-6">
+                                        <div className="size-fit">
+                                            <img
+                                                src={authUser.profilePic}
+                                                className="size-10 rounded-full"
+                                            />
+                                        </div>
+                                        <div>
+                                            <h1 className="text-right text-sm font-semibold">{message.createdAt}</h1>
+                                            {message.image && <div className="size-36 mt-3">
+                                                <img src={message.image} alt="image" className="size-fit object-cover" />
+                                            </div>}
+                                            <div className="bg-primary/25 h-fit mt-3 p-4 rounded-2xl max-w-sm">
+                                                <p>{message.text}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div
+                                    key={index}
+                                    className={`w-full flex justify-start`}
+                                >
+                                    <div className="w-fit flex gap-x-4 mb-6">
+                                        <div className="size-fit">
+                                            <img
+                                                src={selectedUser.profilePic}
+                                                className="size-10 rounded-full"
+                                            />
+                                        </div>
+                                        <div>
+                                            <h1 className="text-left text-sm font-semibold">{message.createdAt}</h1>
+                                            <div className="bg-mediumDark h-fit mt-3 p-4 rounded-2xl max-w-sm">
+                                                <p>{message.text}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        ))}
                     </div>
                 )
             )}
-        </>
+        </div>
     )
 }
