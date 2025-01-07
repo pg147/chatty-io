90005da2c5086276d215be9c4621daa2d9a9001f
pg147 <xyzprathamesh147@gmail.com>
2025-01-07 20:24:12 +0530

    Styling improvements : Chat Interface

--- frontend/src/components/ChatInterface.jsx
+++ frontend/src/components/ChatInterface.jsx
@@ -1,5 +1,5 @@
 // React Imports
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // Global States
 import { useChatStore } from "../store/useChatStore";
@@ -11,21 +11,32 @@ import MessageInputs from "./MessageInputs";
 
 export default function ChatInterface() {
   const { selectedUser, getMessages } = useChatStore();
+  const messagesRef = useRef(null);
 
   useEffect(() => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight
+    }
+
     getMessages(selectedUser._id);
   }, [getMessages, selectedUser._id]);
 
   return (
-    <div className="relative h-[100vh]">
-      {/* User Info */}
-      <ChatHeader />
+    <div className="relative h-screen flex flex-col">
+      {/* Fixed Chat Header */}
+      <div className="sticky top-0 z-10">
+        <ChatHeader />
+      </div>
 
-      {/* Messages */}
-      <Messages />
+      {/* Scrollable Messages */}
+      <div ref={messagesRef} className="flex-grow overflow-y-auto">
+        <Messages />
+      </div>
 
-      {/* User Controls */}
-      <MessageInputs />
+      {/* Fixed Message Inputs */}
+      <div className="sticky bottom-0 z-10 w-full">
+        <MessageInputs />
+      </div>
     </div>
   )
 }
