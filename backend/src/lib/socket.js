eb32d0dd5d06dfee81ec9f99d4332e9e26e10e36
pg147 <xyzprathamesh147@gmail.com>
2025-01-08 00:42:24 +0530

    Implemented Socket Map for Online users

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,12 +12,27 @@ const io = new Server(server, {
     }
 });
 
+// For maintaing online users
+const userSocketMap = {} // {userId (key) : socketId (value)}
+
 io.on("connection", (socket) => {
     console.log("An user is connected : ", socket.id);
+    
+    const userId = socket.handshake.query.userId; // extracting userId from Client Query
+    if (userId) userSocketMap[userId] = socket.id; // updating in userSocketMap
+
+    // io.emit broadcasts events to all Socket Connected users
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     // Event listener for Disconnect
     socket.on("disconnect", () => {
         console.log("The user got disconnected : ", socket.id);
+
+        // Removing the user from map when disconnected
+        delete userSocketMap[userId];
+
+        // Updating the Socket Map after disconnection
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 });
 
