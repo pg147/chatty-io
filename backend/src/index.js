92a42fd90b5ec17fb57e7ba001d8298613129f63
pg147 <xyzprathamesh147@gmail.com>
2025-01-08 22:17:15 +0530

    Production Changes ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import { app, server } from './lib/socket.js';
 
@@ -16,6 +17,7 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config(); // For using env variables
 
 const PORT = process.env.PORT; // defined PORT 
+const __dirname = path.resolve();
 
 // app used here is from socket.js 
 
@@ -34,6 +36,14 @@ app.use(cors({
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 // Socket Server
 server.listen(PORT, () => {
