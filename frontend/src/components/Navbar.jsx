7e4b4b2e2853080b711fccb6b4991dd458b5fb73
pg147 <xyzprathamesh147@gmail.com>
2025-01-11 19:55:03 +0530

    Settings Removed from Navbar and styling updates

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import { useChatStore } from "../store/useChatStore";
 
 // Icon Library
-import { LucideLogOut, LucideMessageSquare, LucideSettings, LucideUser2 } from "lucide-react";
+import { LucideLogOut, LucideMessageSquare, LucideUser2 } from "lucide-react";
 
 export default function Navbar() {
   const { logout, authUser } = useAuthStore();
@@ -18,14 +18,14 @@ export default function Navbar() {
   }
 
   return (
-    <div className="h-fit w-full z-[10] flex items-center justify-between px-4 py-3 lg:px-8 lg:py-6 shadow-intense ">
+    <div className="h-fit w-full z-[10] flex items-center justify-between px-4 py-5 md:px-8 md:py-6 shadow-intense ">
       {/* Brand */}
       <div className="flex items-center gap-x-2.5">
         <div className="h-fit w-fit mx-auto bg-primary/10 rounded-lg p-2.5">
           <LucideMessageSquare className="size-6 text-primary" />
         </div>
         <div>
-          <h1 className="text-lg lg:text-xl font-semibold">Chatty</h1>
+          <h1 className="text-base md:text-xl font-semibold">Chatty</h1>
           <p className="text-sm font-medium">by <span className="font-semibold text-primary">PG</span></p>
         </div>
       </div>
@@ -33,30 +33,28 @@ export default function Navbar() {
       {/* User Controls */}
       <div className="flex w-fit gap-x-3">
         {/* Settings */}
-        <div className="cursor-pointer lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
-          <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
-            <LucideSettings className="size-6 lg:size-5 text-primary" />
+        {/* <div className="cursor-pointer md:px-5 md:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
+          <div className="h-fit w-fit p-3 md:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
+            <LucideSettings className="size-6 md:size-5 text-primary" />
           </div>
           <h1 className="font-medium hidden lg:block">Settings</h1>
-        </div>
+        </div> */}
 
         {authUser && <>
           {/* Profile */}
           <Link to={"/profile"} >
-            <div className="cursor-pointer lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
-              <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
-                <LucideUser2 className="size-6 lg:size-5 text-primary" />
+            <div className="cursor-pointer md:p-3.5 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
+              <div className="h-fit w-fit p-3 md:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
+                <LucideUser2 className="size-5 text-primary" />
               </div>
-              <h1 className="font-medium hidden lg:block">Profile</h1>
             </div>
           </Link>
 
           {/* Logout */}
-          <div onClick={handleLogout} className="cursor-pointer lg:px-5 lg:py-3 rounded-2xl border-[1.5px] border-stroke flex items-center gap-x-2.5 bg-light">
-            <div className="h-fit w-fit p-3 lg:p-0 rounded-full bg-[#FCFCFC] lg:bg-transparent">
-              <LucideLogOut className="size-6 lg:size-5 text-primary" />
+          <div onClick={handleLogout} className="cursor-pointer md:p-3.5 rounded-2xl flex items-center gap-x-2.5 bg-primary/10">
+            <div className="h-fit w-fit p-3 md:p-0 rounded-full">
+              <LucideLogOut className="size-5 text-primary" />
             </div>
-            <h1 className="font-medium hidden lg:block">Logout</h1>
           </div>
         </>}
       </div>
