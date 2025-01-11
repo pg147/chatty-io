559dfb7736f199c013a7a606e590e70df62d09d5
pg147 <xyzprathamesh147@gmail.com>
2025-01-11 18:03:44 +0530

    Styling updated for mobile view

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -40,27 +40,27 @@ export default function Messages() {
                         <h1 className="text-center text-lg">Type &quot;Hi&quot; to start a conversation with <span className="font-semibold">{`${selectedUser.name.slice(0, 1).toUpperCase()}${selectedUser.name.slice(1)}`}</span></h1>
                     </div>
                 ) : (
-                    <div className="flex-1 flex flex-col p-9">
+                    <div className="flex-1 flex flex-col p-3 md:p-9">
                         {messages.map((message, index) => (
                             <div
                                 key={index}
                                 ref={messageRef}
                                 className={`w-full flex ${authUser._id === message.senderId ? 'justify-end' : 'justify-start'}`}
                             >
-                                <div className={`w-fit flex items-end gap-x-4 mb-6 ${authUser._id === message.senderId ? ' flex-row-reverse' : ''}`}>
+                                <div className={`w-fit flex items-end gap-x-2.5 md:gap-x-4 mb-3 md:mb-6 ${authUser._id === message.senderId ? ' flex-row-reverse' : ''}`}>
                                     <div className="size-fit">
                                         <img
                                             src={authUser._id === message.senderId ? authUser.profilePic : selectedUser.profilePic}
-                                            className="size-10 rounded-full"
+                                            className="size-7 md:size-10 rounded-full"
                                         />
                                     </div>
                                     <div>
-                                        <h1 className={`${authUser._id === message.senderId ? 'text-right' : 'text-left'} text-sm text-subtitle font-regular`}>{formatMessageTime(message.createdAt)}</h1>
-                                        <div className={`h-fit mt-3 p-4 rounded-2xl max-w-sm font-regular ${authUser._id === message.senderId ? 'bg-primary/90' : 'bg-mediumDark'}`}>
+                                        <h1 className={`${authUser._id === message.senderId ? 'text-right' : 'text-left'} text-xs md:text-sm text-subtitle font-regular`}>{formatMessageTime(message.createdAt)}</h1>
+                                        <div className={`h-fit mt-2 md:mt-3 p-3 md:p-4 rounded-2xl max-w-xs md:max-w-sm font-regular ${authUser._id === message.senderId ? 'bg-primary/90' : 'bg-mediumDark'}`}>
                                             {message.image && <div className="size-36 mt-3">
                                                 <img src={message.image} alt="image" className="object-cover" />
                                             </div>}
-                                            {message.text && <p className={`${authUser._id === message.senderId ? 'text-white' : 'text-black'}`}>{message.text}</p>}
+                                            {message.text && <p className={`text-sm md:text-base ${authUser._id === message.senderId ? 'text-white' : 'text-black'}`}>{message.text}</p>}
                                         </div>
                                     </div>
                                 </div>
